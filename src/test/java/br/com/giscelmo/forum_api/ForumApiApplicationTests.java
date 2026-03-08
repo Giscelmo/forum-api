a0af3727aa900654fe228b8ef467c45984ec441a
Giscelmo Costa <giscelmo.costa@gmail.com>
2026-03-08 10:56:45 -0300

    chore: configuração inicial do projeto com Spring Boot e Maven

---
+++ src/test/java/br/com/giscelmo/forum_api/ForumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.giscelmo.forum_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
